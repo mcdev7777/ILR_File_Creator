7f2b7b8b0ed3adeedda465f6972500597c4d1909
mcdev7777 <minwangdev7777@outlook.com>
2024-10-16 16:57:46 +0100

    takes cvs file and packages can also make xml file

--- main.js
+++ main.js
@@ -1,28 +1,79 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, globalShortcut } = require('electron');
+const path = require('node:path')
+const fs = require('fs');
+const xmlbuilder = require('xmlbuilder');
+let testObject = {
+  Header:{
+  CollectionDetails:{
+      Collection:"ILR",
+      Year:"2324",
+      FilePreparationDate:"2024-09-11"
+  },
+  Source:{
+      ProtectiveMarking:"OFFICIAL-SENSITIVE-Personal",
+      UKPRN:"10085696",
+      SoftwareSupplier:"Education & Skills Funding Agency",
+      SoftwarePackage:"ILR Learner Entry",
+      Release:"2324.1.92.0",
+      SerialNo:"01",
+      DateTime:"2024-09-11T05:09:12"
+  },
+  },
+  LearningProvider:{
+      UKPRN:"10085696"
+  },
+  
+  }
+
 function createWindow() {
     const win = new BrowserWindow({
       width: 800,
       height: 600,
       webPreferences: {
-        // preload: path.join(__dirname, 'renderer.js'),
-        nodeIntegration: true, // Allows Node.js modules in the renderer process
+        preload: path.join(__dirname, 'renderer.js'),
+        nodeIntegration: true, 
         contextIsolation: false,
+        // these are bad practice and I should change if time can set isolation to true and set a file allowing only the apis I know I want to use
       },
     });
   
     win.loadFile('index.html');
+
+    globalShortcut.register('CommandOrControl+Shift+I', () => {
+      win.webContents.toggleDevTools();
+    });
   }
 
-// let form = document.getElementById('uploadForm');
+
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+ipcMain.on('upload-csv', (event, dataArray) => {
+  console.log('upload-csv event');
+  console.log(dataArray);
+  
+  // Create XML from testObject
+  const xml = xmlbuilder.create(testObject).end({ pretty: true });
+  
+  fs.writeFile("data.xml", xml, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("The XML file was saved successfully.");
+    }
+  }); 
+});
 
 
 
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault();
 
-//     let file = document.getElementById('csvFile').files[0];
-//     console.log(file);
-    
-// });
 
-app.whenReady().then(createWindow);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
---
+++ renderer.js
@@ -0,0 +1,20 @@
+const { ipcRenderer } = require('electron');
+const Papa = require('papaparse'); 
+
+
+
+ const form = document.getElementById('uploadForm');
+ const output = document.getElementById('output');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let file = document.getElementById('csvFile').files[0];
+    Papa.parse(file, {
+        complete: (results) => {
+            const dataArray = results.data;
+             ipcRenderer.send('upload-csv', dataArray);
+        }
+      });
+   
+    
+});
