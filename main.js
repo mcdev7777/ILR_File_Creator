5c2aecec607978c7cbc54598cd78d5f2ffdfbbf5
mcdev7777 <minwangdev7777@outlook.com>
2024-12-05 16:57:02 +0000

    tweaks

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// const {xsd} = require("./schema-for-validate")
 const xmllint = require('xmllint');
 const { app, BrowserWindow, ipcMain, globalShortcut, dialog, shell  } = require("electron");
 const path = require("node:path");
@@ -19,7 +18,7 @@ process.on('uncaughtException', (error) => {
     log.error('Uncaught Exception:', error);
 });
 
-const tempDir = path.join(os.tmpdir(), `electron-${app.name}-xmls`);
+const tempDir = path.join(os.tmpdir(), `electron-ilr_file_creator-xmls`);
 let XMLfilePath = ""
 let versionForExport = ""
 const formatDateTime = (date) => {
@@ -89,22 +88,7 @@ app.whenReady().then(() => {
 
 ipcMain.on("upload-csv", (event, dataArray, version) => {
   try {
-    versionForExport = version
-    if (dataArray.some((learner, learnerIndex) => 
-    learner.some((item, index) => {
-      let exceptionIndices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192,193];
-    if (item === "" && !exceptionIndices.includes(index)) {
-        const missingField = dataArray[0][index] || `Field at index ${index}`;
-        event.reply('show-alert', `Data missing: ${missingField} for learner ${learnerIndex}`);
-        return true;
-      }
-      return false;
-    })
-  )) {
-    event.reply('show-alert', 'Please fill in all required fields');
-  }
-else{ 
-  // test this works and require it
+  versionForExport = version
   xmlBase.Header.Source.Release = version
   xmlBase.Header.CollectionDetails.Year = version.split('.')[0];
   let refNumber = 0
@@ -557,7 +541,7 @@ worker.on('exit', (code) => {
 
   
 
-}
+
 } catch (error) {
   console.error("An error occurred during XML validation:", error);
 }
