059afda0e8ec7c0da377c5a21a5488875aed4fba
mcdev7777 <minwangdev7777@outlook.com>
2025-08-27 17:24:59 +0100

    format: increase readability of renderer.js

--- renderer.js
+++ renderer.js
@@ -1,65 +1,66 @@
-const { ipcRenderer, ipcMain } = require('electron');
-const Papa = require('papaparse'); 
-
+const { ipcRenderer, ipcMain } = require("electron");
+const Papa = require("papaparse");
 
 function logToMain(message) {
-  ipcRenderer.send('log-message', message);
+  ipcRenderer.send("log-message", message);
 }
 
- const form = document.getElementById('uploadForm');
- const output = document.getElementById('output');
+const form = document.getElementById("uploadForm");
+const output = document.getElementById("output");
 
-ipcRenderer.on('show-alert', (event, message) => {
+ipcRenderer.on("show-alert", (event, message) => {
   alert(message);
 });
-const saveButton = document.createElement('button');
-saveButton.textContent = 'Save XML File';
+
+const saveButton = document.createElement("button");
+saveButton.textContent = "Save XML File";
 saveButton.onclick = async () => {
-  ipcRenderer.send("openSave")
+  ipcRenderer.send("openSave");
 };
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let version = document.getElementById('releaseVersion').value;
-    let file = document.getElementById('csvFile').files[0];
-    Papa.parse(file, {
-        complete: (results) => {
-            const dataArray = results.data;
-             ipcRenderer.send('upload-csv', dataArray, version);
-            
-        }
-      });
-   
-    logToMain('submit completed')
+  let version = document.getElementById("releaseVersion").value;
+  let file = document.getElementById("csvFile").files[0];
+
+  Papa.parse(file, {
+    complete: (results) => {
+      const dataArray = results.data;
+      ipcRenderer.send("upload-csv", dataArray, version);
+    },
+  });
+
+  logToMain("submit completed");
 });
 
-ipcRenderer.on('xml-created', (event, filename) => {
-  
-  const outputDiv = document.getElementById('output');
-  const downloadLink = document.createElement('a');
+ipcRenderer.on("xml-created", (event, filename) => {
+  const outputDiv = document.getElementById("output");
+  const downloadLink = document.createElement("a");
+
   outputDiv.appendChild(saveButton);
-logToMain('xml creation completed');
+
+  logToMain("xml creation completed");
 });
 
-ipcRenderer.on('xml-creation-failed', (event, errorMessage) => {
-  const outputDiv = document.getElementById('output');
+ipcRenderer.on("xml-creation-failed", (event, errorMessage) => {
+  const outputDiv = document.getElementById("output");
   outputDiv.textContent = `Error creating XML: ${errorMessage}`;
 });
 
-ipcRenderer.on('xml-validation-errors',(event,results) => {
+ipcRenderer.on("xml-validation-errors", (event, results) => {
   const errorDisplay = document.getElementById("formatErrors");
-  errorDisplay.innerHTML = '';
-  const h2 = document.createElement('h2')
-  h2.textContent = "Errors and Warnings"
+  errorDisplay.innerHTML = "";
+  const h2 = document.createElement("h2");
+  h2.textContent = "Errors and Warnings";
   errorDisplay.appendChild(h2);
-    errorDisplay.appendChild(document.createElement('br'));
-  h2.textContent = "Errors and Warnings"
-  results.errors.forEach(error => {
-    const p = document.createElement('p');
+  errorDisplay.appendChild(document.createElement("br"));
+  h2.textContent = "Errors and Warnings";
+  results.errors.forEach((error) => {
+    const p = document.createElement("p");
     p.textContent = error;
     errorDisplay.appendChild(p);
-    errorDisplay.appendChild(document.createElement('br'));
+    errorDisplay.appendChild(document.createElement("br"));
   });
-  logToMain('xml validation completed');
+  logToMain("xml validation completed");
 });
