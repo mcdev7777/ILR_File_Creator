5b59216bd459cc09fd762c42052d1a83d047b53b
mcdev7777 <minwangdev7777@outlook.com>
2025-08-28 16:40:19 +0100

    format: neaten the xmlValidator code

--- xmlValidator.js
+++ xmlValidator.js
@@ -1,15 +1,21 @@
-const log = require('electron-log');
-log.info('worker running')
-const { parentPort, workerData } = require('worker_threads');
-const xmllint = require('xmllint');
-if(xmllint){log.info('xmllint defined')}
-    const { xml, xsd } = workerData;
+const log = require("electron-log");
+log.info("worker running");
+const { parentPort, workerData } = require("worker_threads");
+const xmllint = require("xmllint");
+if (xmllint) {
+  log.info("xmllint defined");
+}
+const { xml, xsd } = workerData;
 
 try {
-    let result = xmllint.validateXML({ xml, schema: xsd });
-    log.info('result in worker ', result)
-    parentPort.postMessage({ valid: !result.errors || result.errors.length === 0, errors: result.errors });
-    log.info('error not occuring in post messages')
+  let result = xmllint.validateXML({ xml, schema: xsd });
+  log.info("result in worker ", result);
+
+  parentPort.postMessage({
+    valid: !result.errors || result.errors.length === 0,
+    errors: result.errors,
+  });
+  log.info("error not occuring in post messages");
 } catch (error) {
-    parentPort.postMessage({ valid: false, errors: [error.message] });
-} 
+  parentPort.postMessage({ valid: false, errors: [error.message] });
+}
